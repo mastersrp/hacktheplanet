a923b1749a3538c8880b84257a69fe71d0061a9f
T3hGamerDK <tcg.thegamer@gmail.com>
2011-09-29 21:51:39 +0200

    Updated to use OpenGL and freeGLUT

--- src/hacktheplanet.cpp
+++ src/hacktheplanet.cpp
@@ -2,11 +2,34 @@
     This file is part of the HackThePlanet project.
     License is in COPYING file.
 */
+// STD LIBS
 #include <iostream>
+// OPENGL
+#include <GL/gl.h>
+#include <GL/glut.h>
+#ifdef __WIN32
+#   include <GL/wglext.h>
+#else
+#   include <GL/glext.h>
+#endif
+// BOOST C++
 #include <boost/random.hpp>
 #include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 int main( int argc, char *argv[] )
 {
+    boost::property_tree::ptree settings;
+    std::ifstream settings_file;
+    settings_file.open( 'settings.json' );
+    if( !settings_file ) { std::cerr << "Couldn't open 'settings.json'!" << std::endl; return 1; }
+
+    // Graphics init
+    glutInit();
+    glutInitDispayMode( GLUT_SINGLE );
+    glutInitWindowSize( 500, 500 );
+    glutInitWindowPosition(200,200);
+    glutCreateWindow("HackThePlanet");
     return 0;
 }
