21e83e0af178b6c77a48984e148632143ab90bbf
T3hGamerDK <tcg.thegamer@gmail.com>
2011-10-08 04:12:08 +0000

    Added json settings parser

--- src/hacktheplanet.cpp
+++ src/hacktheplanet.cpp
@@ -4,6 +4,8 @@
 */
 // STD LIBS
 #include <iostream>
+#include <fstream>
+#include <string>
 // OPENGL
 #include <GL/gl.h>
 #include <GL/glut.h>
@@ -22,14 +24,21 @@ int main( int argc, char *argv[] )
 {
     boost::property_tree::ptree settings;
     std::ifstream settings_file;
-    settings_file.open( 'settings.json' );
+    settings_file.open("settings.json");
     if( !settings_file ) { std::cerr << "Couldn't open 'settings.json'!" << std::endl; return 1; }
 
+    boost::property_tree::read_json( "settings.json", settings );
+    
+    std::cout << settings.get<std::string>("profile", "default") << std::endl;
+    std::string path_data = settings.get<std::string>("paths.data", "data");
+    std::cout << "path(data): " << path_data << std::endl;
+    /*
     // Graphics init
     glutInit();
     glutInitDispayMode( GLUT_SINGLE );
     glutInitWindowSize( 500, 500 );
     glutInitWindowPosition(200,200);
     glutCreateWindow("HackThePlanet");
+    */
     return 0;
 }
