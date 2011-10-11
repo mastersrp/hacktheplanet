0423837bbcd01784ff67688097895b24d2646d3f
T3hGamerDK <tcg.thegamer@gmail.com>
2011-10-11 12:12:26 +0000

    Added: placeholder json.lua and profilecreator.lua files for future use. Added: globalinclude global lua variable, holding the path to the global lua include folder. Added: onProfileNotFound custom hook, called when the profile could not be found.

--- bin/data/engine.lua
+++ bin/data/engine.lua
@@ -3,10 +3,20 @@
 -- hooks
 -- functions
 -- global variables
+
+-- Includes:
+dofile( globalinclude.."/profilecreator.lua" )
+dofile( globalinclude.."/json.lua" )
+
 function onInit()
 	print( "[LUA] Initiating..." )
 end
 
 function onExit()
 	print( "[LUA] Exiting..." )
+end
+
+function onProfileNotFound()
+	print( "[LUA] Could not find your profile" )
+	print( "[LUA] Creating a new one! (not yet, but this'll come eventually." )
 end
