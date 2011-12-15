7fe32266fbad9572792fbe4fe91c33fce577dcc7
T3hGamerDK <tcg.thegamer@gmail.com>
2011-12-15 12:45:58 +0100

    Converted tabs to spaces.

--- include/htp/util/timer.hpp
+++ include/htp/util/timer.hpp
@@ -5,56 +5,56 @@
 #include <chrono>
 
 namespace HTP {
-    namespace util {
-	class Timer 
+	namespace util {
+		class Timer 
         {
-	    private:
-	        std::chrono::high_resolution_clock::time_point timer_start;
-	        std::chrono::high_resolution_clock::time_point timer_paused;
-	        std::chrono::nanoseconds timer_pausedTicks;
-	        bool paused,started,stopped;
-	    public:
-	        Timer();
-	    
-		void start();
-		void pause();
-		void unpause();
-		std::chrono::nanoseconds get_duration();
-	};
-    }
+			private:
+				std::chrono::high_resolution_clock::time_point timer_start;
+				std::chrono::high_resolution_clock::time_point timer_paused;
+				std::chrono::nanoseconds timer_pausedTicks;
+				bool paused,started,stopped;
+			public:
+				Timer();
+				
+				void start();
+				void pause();
+				void unpause();
+				std::chrono::nanoseconds get_duration();
+		};
+	}
 }
 
 HTP::util::Timer::Timer()
 {
-    paused = false;
-    started = false;
-    stopped = false;
+	paused = false;
+	started = false;
+	stopped = false;
 }
 
 void HTP::util::Timer::start()
 {
-    started = true;
-    paused = false;
-    stopped = false;
-    timer_start = std::chrono::high_resolution_clock::now();
+	started = true;
+	paused = false;
+	stopped = false;
+	timer_start = std::chrono::high_resolution_clock::now();
 }
 
 void HTP::util::Timer::pause()
 {
-    if ( started == true && paused == false )
-    {
-    	paused = true;
-	timer_paused = std::chrono::high_resolution_clock::now();
-    }
+	if ( started == true && paused == false )
+	{
+		paused = true;
+		timer_paused = std::chrono::high_resolution_clock::now();
+	}
 }
 
 void HTP::util::Timer::unpause()
 {
-    if( started == true && paused == true )
-    {
-	paused = false;
-	timer_pausedTicks = std::chrono::high_resolution_clock::now()-timer_paused;
-    }
+	if( started == true && paused == true ) 
+	{
+		paused = false;
+		timer_pausedTicks = std::chrono::high_resolution_clock::now()-timer_paused;
+	}
 }
 std::chrono::nanoseconds HTP::util::Timer::get_duration()
 {
--- src/htp/hacktheplanet.cpp
+++ src/htp/hacktheplanet.cpp
@@ -29,59 +29,47 @@
 
 int main( int argc, char *argv[] )
 {
-    HTP::util::Timer timer_loadtime;
-    // Lua initialization
-    std::cout << "[*I Initializing lua" << std::endl;
-    timer_loadtime.start();
-    lua_State *scriptEngine = script::createState();
-    // Lua scriptHook creation
-    script::hook scriptHook;
-    // Settings parsing
-    std::cout << "[*] Loading settings...";
-    boost::property_tree::ptree settings;
-    std::ifstream settings_file;
-    settings_file.open("data/settings.json");
+	HTP::util::Timer timer_loadtime;
+	// Lua initialization
+	std::cout << "[*I Initializing lua" << std::endl;
+	timer_loadtime.start();
+	auto *scriptEngine = script::createState();
+	// Lua scriptHook creation
+	script::hook scriptHook;
+	// Settings parsing
+	std::cout << "[*] Loading settings...";
+	boost::property_tree::ptree settings;
+	std::ifstream settings_file;
+	settings_file.open("data/settings.json");
     if( !settings_file ) {
-	std::cerr << "Couldn't open 'settings.json'!" << std::endl;
-	scriptHook.onExit(scriptEngine);
-	script::endState(scriptEngine);
-	return 1;
-    }
-    std::cout << "Done!" << std::endl;
-    boost::property_tree::read_json( "data/settings.json", settings );
-    std::string profile = settings.get<std::string>("profile", "default");
-    std::cout << "[i] Using '" << profile << "' profile." << std::endl;
-    std::string enginelua = settings.get<std::string>("profiles."+settings.get<std::string>(profile,"default")+".engine", "data/lua/engine.lua" );
-    // Quick fix for the globalincludes variable.
-    lua_pushstring( scriptEngine, settings.get<std::string>("profiles."+settings.get<std::string>(profile,"default")+".include", "data/lua/include").c_str() );
-    lua_setglobal( scriptEngine, "globalincludes" );
-    std::cout << "[*] Loading lua framework..." << std::endl;
-    script::report_errors( scriptEngine, luaL_dofile(scriptEngine, enginelua.c_str()) );
-    std::cout << "[i] Loading took " << timer_loadtime.get_duration().count() << " nanoseconds." << std::endl;
+		std::cerr << "Couldn't open 'settings.json'!" << std::endl;
+		scriptHook.onExit(scriptEngine);
+		script::endState(scriptEngine);
+		return 1;
+	}
+	std::cout << "Done!" << std::endl;
+	boost::property_tree::read_json( "data/settings.json", settings );
+	std::string profile = settings.get<std::string>("profile", "default");
+	std::cout << "[i] Using '" << profile << "' profile." << std::endl;
+	std::string enginelua = settings.get<std::string>("profiles."+settings.get<std::string>(profile,"default")+".engine", "data/lua/engine.lua" );
+	// Quick fix for the globalincludes variable.
+	lua_pushstring( scriptEngine, settings.get<std::string>("profiles."+settings.get<std::string>(profile,"default")+".include", "data/lua/include").c_str() );
+	lua_setglobal( scriptEngine, "globalincludes" );
+	std::cout << "[*] Loading lua framework..." << std::endl;
+	script::report_errors( scriptEngine, luaL_dofile(scriptEngine, enginelua.c_str()) );
+	std::cout << "[i] Loading took " << timer_loadtime.get_duration().count() << " nanoseconds." << std::endl;
 	
-    HTP::render::App App;
-    if( App.init() == false ) {
-    	return 1;
-    }
-    timer_loadtime.start();
-    scriptHook.onInit( scriptEngine );
-    std::cout << "[i] Init took " << timer_loadtime.get_duration().count() << " nanoseconds." << std::endl;
-    script::dofunction( scriptEngine, "init" );
-
-    std::cout << "[i] Running main rendering/interactive loop." << std::endl;
-    bool running = App.running();
-    initscr();
-//    while( running )
-//    {
-	printw( "Hello world!" );
-	refresh();
-	App.ProcessInput();
-	running = App.running();
-//    }
-    endwin();
-    // Ending scriptHook
-    scriptHook.onExit( scriptEngine );
-    // Ending lua scriptEngine
-    script::endState(scriptEngine);
-    return 0;
+	HTP::render::App App;
+	if( App.init() == false ) {
+		return 1;
+	}
+	timer_loadtime.start();
+	scriptHook.onInit( scriptEngine );
+	std::cout << "[i] Init took " << timer_loadtime.get_duration().count() << " nanoseconds." << std::endl;
+	script::dofunction( scriptEngine, "init" );
+	// Ending scriptHook
+	scriptHook.onExit( scriptEngine );
+	// Ending lua scriptEngine
+	script::endState(scriptEngine);
+	return 0;
 }
