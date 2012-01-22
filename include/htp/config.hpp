45de4812fcd0d47219874485fc3e3b61ac14fa6f
T3hGamerDK <tcg.thegamer@gmail.com>
2012-01-22 01:07:09 +0100

    Updated configuration.

--- include/htp/config.hpp
+++ include/htp/config.hpp
@@ -8,6 +8,9 @@
 #define HTP_USE_GL			// irrLicht + OpenGL
 //#define HTP_USE_GWEN		// Skinable GUI system written by the creator of Garry's Mod; Garry Newman
 
+#define SCRIPT_USE_CUSTOM_CONFIG
+#define SCRIPT_USE_AS // Use AngelScript for our script engine
+
 /* CONFIGURATION LISTED ABOVE */
 
 #endif /* CONFIG_HPP */
